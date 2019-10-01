ed6040830ee528effc614210467039b7ef314aa7
bpiotrowski <baej.p26@gmail.com>
2019-10-01 14:19:21 +0200

    zad 2 - poprawienie usuwania

--- src/main/java/pl/bpiotrowski/servlet/TodoController.java
+++ src/main/java/pl/bpiotrowski/servlet/TodoController.java
@@ -16,7 +16,6 @@ public class TodoController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-
         if(session.getAttribute("todoList") == null) {
             List<Todo> todoList = new ArrayList<>();
             session.setAttribute("todoList", todoList);
@@ -30,6 +29,20 @@ public class TodoController extends HttpServlet {
             resp.sendRedirect("todos");
             return;
         }
+
+        String taskToDeleteUuid = req.getParameter("taskToDelete");
+        if(taskToDeleteUuid != null) {
+            List<Todo> todoList = (List<Todo>) req.getSession().getAttribute("todoList");
+            if(todoList != null) {
+                for (Todo td : todoList) {
+                    if (td.getUuid().equals(taskToDeleteUuid)) {
+                        todoList.remove(td);
+                        break;
+                    }
+                }
+            }
+        }
+
         req.getRequestDispatcher("todos.jsp").forward(req, resp);
     }
 
