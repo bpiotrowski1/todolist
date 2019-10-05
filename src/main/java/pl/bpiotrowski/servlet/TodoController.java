7e96420334a873a3b8e62437dbe81a124dd5875b
bpiotrowski1 <baej.p26@gmail.com>
2019-10-05 10:48:17 +0200

    logger - konfiguracja

--- src/main/java/pl/bpiotrowski/servlet/TodoController.java
+++ src/main/java/pl/bpiotrowski/servlet/TodoController.java
@@ -1,5 +1,7 @@
 package pl.bpiotrowski.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.bpiotrowski.entity.Todo;
 
 import javax.servlet.ServletException;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @WebServlet("/todos")
 public class TodoController extends HttpServlet {
+    private static Logger log = LoggerFactory.getLogger(TodoController.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
